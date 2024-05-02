e50002ddadd9f68a1820250bd375fe427d35377c
turegjorup <ture@ignatz.dk>
2024-05-02 09:42:29 +0200

    1282: Added Api key auth

---
+++ src/Security/ApiUser.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Security;
+
+use Symfony\Component\Security\Core\User\UserInterface;
+
+class ApiUser implements UserInterface
+{
+    public function __construct(public readonly string $username)
+    {
+    }
+
+    public function getRoles(): array
+    {
+        return ['ROLE_USER'];
+    }
+
+    public function eraseCredentials(): void
+    {
+        // Do nothing
+    }
+
+    public function getUserIdentifier(): string
+    {
+        return $this->username;
+    }
+}
---
+++ src/Security/ApiUserProvider.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Security;
+
+use Symfony\Component\Security\Core\Exception\UserNotFoundException;
+use Symfony\Component\Security\Core\User\UserInterface as TUser;
+use Symfony\Component\Security\Core\User\UserProviderInterface;
+
+/**
+ * @implements UserProviderInterface<TUser>
+ */
+class ApiUserProvider implements UserProviderInterface
+{
+    private array $users = [];
+
+    public function __construct(private array $apikeys = [])
+    {
+        foreach ($apikeys as $apikey) {
+            $this->users[$apikey['apikey']] = new ApiUser($apikey['username']);
+        }
+    }
+
+    public function refreshUser(TUser $user): void
+    {
+        // Do nothing
+    }
+
+    public function supportsClass(string $class): bool
+    {
+        return ApiUser::class === $class;
+    }
+
+    public function loadUserByIdentifier(string $identifier): TUser
+    {
+        if (array_key_exists($identifier, $this->users)) {
+            return $this->users[$identifier];
+        }
+
+        throw new UserNotFoundException();
+    }
+}
